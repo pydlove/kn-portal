6ef64459b985ca4f5f6216cbc6c3d8b23af0a353
yangbin <bin.yang23@gientech.com>
2025-03-02 20:12:05 +0800

    feat://优化数字匹配正则

--- ui-console/src/main/java/com/aiocloud/onetable/console/nlp/parse/KeywordSpliter.java
+++ ui-console/src/main/java/com/aiocloud/onetable/console/nlp/parse/KeywordSpliter.java
@@ -24,15 +24,15 @@ public class KeywordSpliter {
 
     public static final String limitRegex = "(显示|显示前|显示后|显示最后|需要|需要前|需要后|需要最后|展示|展示前|展示后|展示最后|查询|查询前|查询后|查询最后|取|取前|取后|取最后|获取|获取前|获取后|获取最后|给我|给我前|给我后|给我最后|找出|找出前|找出后|找出最后|找|找前|找后|找最后|查出|查出前|查出后|查出最后|查|查前|查后|查最后)+[0-9]+[条行]";
     private static final String ageRegex = "[0-9零一二贰两三叁四肆五伍六陆七柒八捌九玖十拾百佰千仟万萬亿]+[岁]";
-    private static final String zhcnNumRegex = "[零一二贰两三叁四肆五伍六陆七柒八捌九玖十拾百佰千仟万萬亿]";
+    private static final String zhcnNumRegex = "[零一壹二贰两三叁四肆五伍六陆七柒八捌九玖十拾百佰千仟万萬亿]+";
 
     public static void main(String[] args) {
         String text = "查询十八岁,二十七岁";
-        Pattern pattern = Pattern.compile(ageRegex);
+        Pattern pattern = Pattern.compile(zhcnNumRegex);
         Matcher matcher = pattern.matcher(text);
         if (matcher.find()){
             System.out.println(matcher.group());
-            System.out.println(NumberConverter.convert("一十二"));
+            System.out.println(NumberConverter.convert("十二"));
         }
     }
 
@@ -60,10 +60,16 @@ public class KeywordSpliter {
         }
         if (content.contains("大于" + ageGroup) || content.contains(ageGroup + "以上")
                 || content.contains(ageGroup + "以后") || content.contains(ageGroup + "后")){
-            content = content.replace(ageGroup, "年龄大于" + ageText);
+            content = content.replace("大于" + ageGroup, "年龄大于" + ageText)
+                    .replace(ageGroup + "以上", "年龄大于" + ageText)
+                    .replace(ageGroup + "以后", "年龄大于" + ageText)
+                    .replace(ageGroup + "后", "年龄大于" + ageText);
         } else if (content.contains("小于" + ageGroup) || content.contains(ageGroup + "以下")
                 || content.contains(ageGroup + "以前") || content.contains(ageGroup + "前")){
-            content = content.replace(ageGroup, "年龄小于" + ageText);
+            content = content.replace("小于" + ageGroup, "年龄小于" + ageText)
+                    .replace(ageGroup + "以下", "年龄小于" + ageText)
+                    .replace(ageGroup + "以前", "年龄小于" + ageText)
+                    .replace(ageGroup + "前", "年龄小于" + ageText);
         } else {
             content = content.replace(ageGroup, "年龄等于" + ageText);
         }
